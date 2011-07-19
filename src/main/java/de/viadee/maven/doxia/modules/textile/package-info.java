1778de3a0324bc710a856419e88d26080042ed43
Sebastian Hoß <mail@shoss.de>
2011-07-19 12:01:03 +0200

    Added package documentation.
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

---
+++ src/main/java/de/viadee/maven/doxia/modules/textile/package-info.java
@@ -0,0 +1,31 @@
+/*
+ * Project: doxia-module-textile
+ * Package: de.viadee.maven.doxia.modules.textile
+ * File   : package-info.java
+ * Created: Jul 19, 2011 - 11:59:27 AM
+ *
+ *
+ * Copyright 2011 viadee IT Unternehmensberatung GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+/**
+ * This package contains all Textile related Doxia files.
+ *
+ * @author  Sebastian Hoß (sebastian.hoss@viadee.de)
+ * @see     <a href="https://github.com/viadee/doxia-module-textile">https://github.com/viadee/doxia-module-textile</a>
+ * @since   1.0.0
+ */
+package de.viadee.maven.doxia.modules.textile;
+
